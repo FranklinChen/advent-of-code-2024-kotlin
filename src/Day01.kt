cf67479fc02ea863dd853d4a1a326e36a99c3195
Franklin Chen <franklinchen@franklinchen.com>
2024-12-07 23:24:08 -0500

    Day 01, part 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,16 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // Parse the input into two lists, v0 and v1.
+        val white = "\\s+".toRegex()
+        val (v0, v1) = input.map { line ->
+            line.split(white).map { it.toInt() }.let { it[0] to it[1] }
+        }.unzip()
+
+        // Sort both lists.
+        val sortedV0 = v0.sorted()
+        val sortedV1 = v1.sorted()
+
+        return sortedV0.zip(sortedV1).sumOf { (n0, n1) -> kotlin.math.abs(n0 - n1) }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +18,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
